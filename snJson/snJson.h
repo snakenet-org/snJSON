0fce46eee410cb20870455796d3b78a668473396
Marc Fiedler <marc@snakenet.org>
2015-07-09 10:54:06 +0200

    Initial Commit

---
+++ snJson/snJson.h
@@ -0,0 +1,19 @@
+//
+//  snJson.h
+//  snJson
+//
+//  Created by Marc Fiedler on 09/07/15.
+//  Copyright (c) 2015 snakeNet.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for snJson.
+FOUNDATION_EXPORT double snJsonVersionNumber;
+
+//! Project version string for snJson.
+FOUNDATION_EXPORT const unsigned char snJsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <snJson/PublicHeader.h>
+
+
